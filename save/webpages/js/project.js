8edcd213ffebe2f054aa6a39ca4d8fbb18b19d8e
Andre Herzog <andre.herzog@gbv.de>
2022-02-23 12:13:52 +0100

    Added instance urls to project.js

--- save/webpages/js/project.js
+++ save/webpages/js/project.js
@@ -19,8 +19,13 @@ $(document).ready(function() {
 
   // replace placeholder USERNAME with username
   var userID = $("#currentUser strong").html();
-  var newHref = 'https://reposis-test.gbv.de/PROJECT/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='https://reposis-test.gbv.de/PROJECT/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
+  var localHref = 'http://localhost:18021/phtg/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='http://localhost:18021/phtg/servlets/solr/select?q=createdby:USERNAME']").attr('href', localHref);
+  var testHref = 'https://reposis-test.gbv.de/phtg/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://reposis-test.gbv.de/phtg/servlets/solr/select?q=createdby:USERNAME']").attr('href', testHref);
+  var prodHref = 'https://www.publikationsserver.phtg.ch/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://www.publikationsserver.phtg.ch/servlets/solr/select?q=createdby:USERNAME']").attr('href', prodHref);
+
 
   // unhide person extended box at reload
   $('.personExtended-container').removeClass('d-none');
